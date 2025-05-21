eadc78ccdc601e425681467ce389bedc1a9fe9b7
Gavin-Fetrow-Student <gafe134@student.oaisd.org>
2025-05-21 10:43:18 -0400

    Cameras

---
+++ Assets/CameraSystem.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Cameras;
+
+    [SerializeField] private int CurrentCam;
+
+    [SerializeField] private KeyCode OpenCameras;
+
+    [SerializeField] private bool CamerasOpen;
+
+    [SerializeField] private GameObject MainCamera;
+
+    [SerializeField] private float CoolDownTimer;
+    [SerializeField] private float CoolDownTime = 0.5f;
+
+    [SerializeField] private GameObject CameraSystemUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < Cameras.Length; i++)
+        {
+            Cameras[i].SetActive(false);
+        }
+        CameraSystemUI.SetActive(false);
+        MainCamera.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(OpenCameras))
+        {
+            CamerasOpen = !CamerasOpen;
+            ShowCamera();
+        }
+
+        if(CoolDownTimer <= 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                Cameras[CurrentCam].SetActive(false);
+                CurrentCam = CurrentCam + 1;
+                if (CurrentCam >= Cameras.Length)
+                {
+                    CurrentCam = 0;
+                }
+                GoToCamera(CurrentCam);
+                CoolDownTimer = CoolDownTime;
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                Cameras[CurrentCam].SetActive(false);
+                CurrentCam = CurrentCam - 1;
+                if (CurrentCam < 0)
+                {
+                    CurrentCam = Cameras.Length - 1;
+                }
+                GoToCamera(CurrentCam);
+                CoolDownTimer = CoolDownTime;
+            }
+        }
+        else
+        {
+            CoolDownTimer -= Time.deltaTime;
+        }
+    }
+
+    private void ShowCamera()
+    {
+        if (CamerasOpen)
+        {
+            Cameras[CurrentCam].SetActive(true);
+            CameraSystemUI.SetActive(true);
+            MainCamera.SetActive(false);
+        }
+        else
+        {
+            Cameras[CurrentCam].SetActive(false);
+            CameraSystemUI.SetActive(false);
+            MainCamera.SetActive(true);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public void GoToCamera(int Progression)
+    {
+        Cameras[CurrentCam].SetActive(false);
+        CurrentCam = Progression;
+        ShowCamera();
+    }
+}
